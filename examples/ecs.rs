dd1a0124aedc84137d797fda7d6803cf02e2a61e
TakWolf <takwolf@foxmail.com>
2020-08-31 19:03:36 +0800

    ecs

--- examples/ecs.rs
+++ examples/ecs.rs
@@ -11,6 +11,16 @@ mod res {
     pub const TEXTURE_TANK_TURRENT: &str = "assets/soldier-tank/turrent.png";
 }
 
+struct KinematicObject {
+    position: Position,
+    angle: Angle,
+    scale: Vector,
+}
+
+struct RotationComponent {
+    angle_speed: Angle,
+}
+
 struct App {
     registry: AssetRegistry,
     world: World,
@@ -23,7 +33,30 @@ impl App {
             .load::<Texture>(engine, res::TEXTURE_TANK_BODY)?
             .load::<Texture>(engine, res::TEXTURE_TANK_TURRENT)?
             .build();
-        let world = World::default();
+        let mut world = World::default();
+        world.extend(vec![
+            (KinematicObject {
+                position: Position::new(100.0, 100.0),
+                angle: Angle::zero(),
+                scale: Vector::new(1.0, 1.0),
+            }, RotationComponent {
+                angle_speed: Angle::radians(0.02),
+            }),
+            (KinematicObject {
+                position: Position::new(200.0, 200.0),
+                angle: Angle::zero(),
+                scale: Vector::new(1.0, 1.0),
+            }, RotationComponent {
+                angle_speed: Angle::radians(0.05),
+            }),
+            (KinematicObject {
+                position: Position::new(300.0, 300.0),
+                angle: Angle::zero(),
+                scale: Vector::new(1.0, 1.0),
+            }, RotationComponent {
+                angle_speed: Angle::radians(0.08),
+            }),
+        ]);
         Ok(Self {
             registry,
             world,
@@ -35,17 +68,37 @@ impl Game for App {
     fn update(&mut self, engine: &mut Engine) -> GameResult {
         let title = format!("{} - FPS: {}", TITLE, engine.timer().real_time_fps().round());
         engine.window().set_title(title);
+
+        let mut query = <(&mut KinematicObject, &RotationComponent)>::query();
+        for (object, component) in query.iter_mut(&mut self.world) {
+            object.angle += component.angle_speed;
+        }
+
         Ok(())
     }
 
     fn render(&mut self, engine: &mut Engine) -> GameResult {
         engine.graphics().clear(Color::BLACK);
 
+        let mut query = <&KinematicObject>::query();
+        for object in query.iter(&self.world) {
+            engine.graphics().draw_sprite(
+                TextureRef::None,
+                SpriteDrawParams::default()
+                    .region((0.0, 0.0, 50.0, 50.0))
+                    .origin((25.0, 25.0)),
+                Transform::default()
+                    .scale(object.scale)
+                    .rotate(object.angle)
+                    .translate(object.position),
+            );
+        }
+
         engine.graphics().draw_text(
             self.registry.font(res::FONT_ROBOTO)?,
             "Tank",
             TextDrawParams::default()
-                .color(Color::BLACK),
+                .color(Color::WHITE),
             Transform::default()
                 .translate((10.0, 10.0)),
         );
